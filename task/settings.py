2f3bf0dc2e3ae32b5326a19b5b0721deb2d5801a
PULLURI SAI LALITH <lalithpulluri@gmail.com>
2021-07-29 13:39:15 +0530

    added env file to secure sensitive data

--- task/settings.py
+++ task/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#(&y$4jaum1vzgq9m=+a@mb=myy4vyt944v76lvz7yp#0cmb2#'
-
+SECRET_KEY = config('SECRET_KEY') 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'task',
-        'USER':'postgres',
-        'PASSWORD':'1234',
+        'NAME': config('NAME'),
+        'USER':config('USER'),
+        'PASSWORD':config('PASSWORD'),
     }
 }
 
