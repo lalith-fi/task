79dd3531a8e8028082d4b7cd03aadc5a16a469e8
PULLURI SAI LALITH <lalithpulluri@gmail.com>
2021-07-29 21:05:15 +0530

    .

--- task/urls.py
+++ task/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('AuthController.urls')),
+    path('auth/', include('AuthController.urls')),
     path('todo/', include('TodoController.urls'))
 ]
