9db9bb4c70df01a6d3341dae18e3346e541f4eb2
PULLURI SAI LALITH <lalithpulluri@gmail.com>
2021-07-29 23:20:31 +0530

    added csrf token

--- AuthController/urls.py
+++ AuthController/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns=[
-    path('register',register_view.as_view(),name="register"), # url for registering a user
-    path('login',login_view.as_view(),name="login"),          # url for logging in a user
+    path('register',csrf_exempt(register_view.as_view()),name="register"), # url for registering a user
+    path('login',csrf_exempt(login_view.as_view()),name="login"),          # url for logging in a user
     path('logout',logout_view.as_view(),name="logout"),       # url for logging out a user
 ]
--- TodoController/urls.py
+++ TodoController/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns=[
     path('getall',getall_view.as_view(),name="getall"),          # url for getting all the todoitems
     path('getone/<int:id>',getone_view.as_view(),name="getone"), # url for getting single todoitem
-    path('create',create_view.as_view(),name="create"),          # url for creating a todoitem
+    path('create',csrf_exempt(create_view.as_view()),name="create"),          # url for creating a todoitem
     path('update/<int:id>',update_view.as_view(),name="update"), # url for updating a todoitem
     path('delete/<int:id>',delete_view.as_view(),name="delete")  # url for deleting a todoitem
 ]
